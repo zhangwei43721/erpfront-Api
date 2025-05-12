e0a94b0b7128af3be3f4e2a510f3ea8ac207e7db
zhangwei43721 <zhangwei4372@outlook.com>
2025-05-12 16:11:34 +0800

    修改启动类使其能扫描mapper

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@MapperScan("com.example.demo.mapper")//添加这句话
 class DemoApplicationTests {
 
     @Test
